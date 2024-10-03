6d0844cd572dbf41e8fb4e9d159cdc518e890b06
Bushrajan <darkprince67890@gmail.com>
2024-10-03 17:59:09 +0500

    done

--- project2/index.js
+++ project2/index.js
@@ -1,178 +1,108 @@
-var weathers = ["Rainy", "Snow", "Cloudy", "Summer", "Fog", "Spring", "Autumn"]
-var user_input = prompt(`"Enter only one Weather : \n Rainy", "Snow", "Cloudy", "Summer", "Fog", "Spring", "Autumn"`)
+
+var Kidz_solutions_array = ["Table", "Forward_Counting", "Backward_Counting"]
+var user_input = +prompt(`Enter your choice: (1-3)\n\n
+(1.) "Generate_Table of any Number"
+(2.) "Generate_Forward_Counting"
+(3.) "Generate_Backward_Counting"
+`)
 var istrue = false;
 
+for (let i = 0; i < Kidz_solutions_array.length; i++) {
+    if (user_input === 1) {
+        istrue = true; document.write(` <div
+    class="container-fluid first p-lg-5 p-lg-5 p-1">
+    <h1 class="text-center fw-bold pt-2  display-4 ">KIDS SOLUTIONS</h1>
+    <h2 class="text-center fw-bold mt-3">***** "Table" *****</h2>
 
-for (let i = 0; i < weathers.length; i++) {
-    if (user_input === "Rainy" || user_input === "rainy") {
-        istrue = true;
-        document.write(` 
-           
-    <div class="container-fluid first p-lg-5 p-lg-5 p-1">
-        <h1 class="text-center fw-bold pt-2  display-4 ">Weather Card</h1>
-        <h5 class="text-center fw-bold mt-3">"Rainy", "Snow", "Cloudy", "Summer", "Fog",
-            "Spring",
-            "Autumn"</h5>
-        
-            <div class="card " style="animation:bounceInDown;  animation-duration:2s;"> 
-            <img src="https://jovelmusic.com/wp-content/uploads/2014/02/tumblr_n0bwlwivnr1qeodauo1_500.gif" alt="Avatar" style="width:100%"> 
-            <div class="container"> 
-            <h4 class="text-center mt-1"><b>🌧️🌧️Rainy Day🌧️🌧️</b></h4> 
-            <p class="text-center mt-1">Lorem ipsum Deserunt minus fugiat itaque voluptates, ea enim.</p> 
-            </div> 
-            </div>
+    <div class="card pt-3 text-center" style="word-spacing: 1em; animation:bounceInDown;  animation-duration:2s;">
+
+        <script>
+
+            var Startno = +prompt("Enter the Table Starting Number:");
+            var Endno = +prompt("Enter the Table Ending Number:");
+            var Tableno = +prompt("Enter Your Table Number:");
+
+            document.write("<table>");
+            for (var i = Startno; i <= Endno; i++) {
+                document.write("<tr><td>" + Tableno + " X " + i + " " + " = " + " " + (Tableno * i) + "</td></tr>");
+            }
+            document.write("</table>");
+
+        </script>
+
+    </div>
     </div>
     `)
         break;
     }
 
-    if (user_input === "Snow" || user_input === "snow") {
+    else if (user_input === 2) {
         istrue = true;
-        document.write(` 
-           
-           
-    <div class="container-fluid first p-lg-5 p-lg-5 p-1">
-        <h1 class="text-center fw-bold pt-2  display-4 ">Weather Card</h1>
-        <h5 class="text-center fw-bold mt-3">"Rainy", "Snow", "Cloudy", "Summer", "Fog",
-            "Spring",
-            "Autumn"</h5>
-             
-                    <div class="card " style="animation:bounceInDown;  animation-duration:2s;"> 
-                    <img src="https://www.icegif.com/wp-content/uploads/2022/01/icegif-1026.gif" alt="Avatar" style="width:100%"> 
-                    <div class="container"> 
-                    <h4 class="text-center mt-1"><b>❄️❄️Snow Day❄️❄️</b></h4> 
-                    <p class="text-center mt-1">Lorem ipsum Deserunt minus fugiat itaque voluptates, ea enim.</p> 
-                    </div> 
-                    </div>
-            </div>
-            `)
-        break;
-    }
+        document.write(`
+    <div class="container-fluid second  p-1">
+        <h1 class="text-center fw-bold pt-2  display-4 ">KIDS SOLUTIONS</h1>
+        <h5 class="text-center fw-bold mt-3">****"Forward_Counting"****</h5>
 
-    else if (user_input === "Fog" || user_input === "fog") {
-        istrue = true;
-        document.write(` 
-               
-                
-    <div class="container-fluid first p-lg-5 p-lg-5 p-1">
-        <h1 class="text-center fw-bold pt-2  display-4 ">Weather Card</h1>
-        <h5 class="text-center fw-bold mt-3">"Rainy", "Snow", "Cloudy", "Summer", "Fog",
-            "Spring",
-            "Autumn"</h5>
-        
-            <div class="card " style="animation:bounceInDown;  animation-duration:2s;"> 
-                        <img src="https://i.makeagif.com/media/10-16-2018/rjxI8k.gif" alt="Avatar" style="width:100%"> 
-                        <div class="container"> 
-                        <h4 class="text-center mt-1"><b>🌫️🌫️Fog Day🌫️🌫️</b></h4> 
-                        <p class="text-center mt-1">Lorem ipsum Deserunt minus fugiat itaque voluptates, ea enim.</p> 
-                        </div> 
-                        </div>
-                </div>
-                `)
-        break;
-    }
+        <div class="card pt-3 text-center"
+            style="word-spacing: 0.2em; animation:bounceInDown;  animation-duration:2s;">
 
-    else if (user_input === "Summer" || user_input === "summer") {
-        istrue = true;
-        document.write(` 
-               
-           
-    <div class="container-fluid first p-lg-5 p-lg-5 p-1">
-        <h1 class="text-center fw-bold pt-2  display-4 ">Weather Card</h1>
-        <h5 class="text-center fw-bold mt-3">"Rainy", "Snow", "Cloudy", "Summer", "Fog",
-            "Spring",
-            "Autumn"</h5>
-        
-                    <div class="card " style="animation:bounceInDown;  animation-duration:2s;"> 
-                    <img src="https://img1.picmix.com/output/pic/normal/4/7/4/3/3373474_92041.gif" alt="Avatar" style="width:100%"> 
-                    <div class="container"> 
-                    <h4 class="text-center mt-1"><b>🏖️🏖️Summer Day🏖️🏖️</b></h4> 
-                    <p class="text-center mt-1">Lorem ipsum Deserunt minus fugiat itaque voluptates, ea enim.</p> 
-                    </div> 
-                    </div>
-            </div>
-            `)
-        break;
-    }
+            <script>
+                var Startfcount = +prompt("Enter the Forward_Counting Starting Number:");
+                var Endfcount = +prompt("Enter the Forward_Counting Ending Number:");
 
-    else if (user_input === "Cloudy" || user_input === "cloudy") {
-        istrue = true;
-        document.write(` 
-               
-            
-    <div class="container-fluid first p-lg-5 p-lg-5 p-1">
-        <h1 class="text-center fw-bold pt-2  display-4 ">Weather Card</h1>
-        <h5 class="text-center fw-bold mt-3">"Rainy", "Snow", "Cloudy", "Summer", "Fog",
-            "Spring",
-            "Autumn"</h5>
-        
-                
-                    <div class="card " style="animation:bounceInDown;  animation-duration:2s;"> 
-                    <img src="https://i.gifer.com/embedded/download/1Unb.gif" alt="Avatar" style="width:100%"> 
-                    <div class="container"> 
-                    <h4 class="text-center mt-1"><b>⛅⛅Cloudy Day⛅⛅</b></h4> 
-                    <p class="text-center mt-1">Lorem ipsum Deserunt minus fugiat itaque voluptates, ea enim.</p> 
-                    </div> 
-                    </div>
-            </div>
-            `)
+                   for (var i = Startfcount; i <= Endfcount; i = i + 10) {
+                       for (var j = i; j < i + 10; j++) {
+                           document.write(" " + j + " ");
+                       }
+                   }
+
+            </script>
+
+        </div>
+    </div>
+    `);
         break;
     }
 
 
-    else if (user_input === "Spring" || user_input === "spring") {
-        istrue = true;
-        document.write(` 
-               
-         
-    <div class="container-fluid first p-lg-5 p-lg-5 p-1">
-        <h1 class="text-center fw-bold pt-2  display-4 ">Weather Card</h1>
-        <h5 class="text-center fw-bold mt-3">"Rainy", "Snow", "Cloudy", "Summer", "Fog",
-            "Spring",
-            "Autumn"</h5>
-        
-                    <div class="card " style="animation:bounceInDown;  animation-duration:2s;"> 
-                    <img src="https://img1.picmix.com/output/pic/normal/5/4/9/1/7551945_6e670.gif" alt="Avatar" style="width:100%"> 
-                    <div class="container"> 
-                    <h4 class="text-center mt-1"><b>🪻🪻Spring Day🪻🪻</b></h4> 
-                    <p class="text-center mt-1">Lorem ipsum Deserunt minus fugiat itaque voluptates, ea enim.</p> 
-                    </div> 
-                    </div>
-            </div>
-            `)
-        break;
-    }
 
-    else if (user_input === "Autumn" || user_input === "autumn") {
+    else if (user_input === 3) {
         istrue = true;
-        document.write(` 
-          
-    <div class="container-fluid first p-lg-5 p-lg-5 p-1">
-        <h1 class="text-center fw-bold pt-2  display-4 ">Weather Card</h1>
-        <h5 class="text-center fw-bold mt-3">"Rainy", "Snow", "Cloudy", "Summer", "Fog",
-            "Spring",
-            "Autumn"</h5>
-        
-                
-                    <div class="card " style="animation:bounceInDown;  animation-duration:2s;"> 
-                    <img src="https://img1.picmix.com/output/pic/normal/7/4/0/8/9328047_cca75.gif" alt="Avatar" style="width:100%"> 
-                    <div class="container"> 
-                    <h4 class="text-center mt-1"><b>🍂🍂Autumn Day🍂🍂</b></h4> 
-                    <p class="text-center mt-1">Lorem ipsum Deserunt minus fugiat itaque voluptates, ea enim.</p> 
-                    </div> 
-                    </div>
-            </div>
-            `)
+        document.write(`
+    <div class="container-fluid third  p-1">
+        <h1 class="text-center fw-bold pt-2  display-4 ">KIDS SOLUTIONS</h1>
+        <h5 class="text-center fw-bold mt-3">****"Backward_Counting"****</h5>
+
+        <div class="card pt-3 text-center"
+            style="word-spacing: 0.2em; animation:bounceInDown;  animation-duration:2s;">
+
+            <script>
+                var Startbcount = +prompt("Enter the Backward_Counting Starting Number:");
+                var Endbcount = +prompt("Enter the Backward_Counting Ending Number:");
+
+                for (var i = Startbcount; i >= Endbcount; i -= 10) {
+                    for (var j = i; j > i - 10; j--) {
+                        document.write(" " + j + " ");
+                    }
+                }
+
+            </script>
+
+        </div>
+    </div>
+    `);
         break;
     }
 
     else {
-        document.write(`<div class="container text-center mt-5 pt-5 p-5"><h1>unvalid input Try again...</h1></div>`)
+        document.write(`<div class="container text-center mt-5 pt-5 p-5">
+        <h1>unvalid input Try again...</h1>
+    </div>`)
         break;
     }
-
 }
 
 if (!istrue) {
-    alert("Not found Any Card")
+    alert("Code not working properly... ");
 }
+
